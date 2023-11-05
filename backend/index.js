0c552570d92019b4213da6164fa2d46c055161ad
Jerico <116884354+cout05@users.noreply.github.com>
2023-11-05 12:48:32 +0800

    Update index.js

--- backend/index.js
+++ backend/index.js
@@ -11,7 +11,7 @@ const app = express();
 
 app.use(
   cors({
-    origin: ["https://todo-app-frontend-rose.vercel.app"],
+    origin: ["https://todo-app-frontend-sigma-swart.vercel.app"],
     methods: ["POST", "GET", "PUT", "DELETE"],
     credentials: true,
   })
