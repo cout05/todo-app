4fa8f526f7f16a919d023f78c6f4388b1bc6b43f
Jerico <116884354+cout05@users.noreply.github.com>
2023-11-17 15:20:39 +0800

    Update index.js

--- backend/index.js
+++ backend/index.js
@@ -1,7 +1,7 @@
 import express from "express";
 import mongoose from "mongoose";
 import cors from "cors";
-import taskRoutes from "./routes/taskRoutes";
+import taskRoutes from "./routes/taskRoutes.js";
 
 const PORT = 5555;
 const MONGO_URI =
