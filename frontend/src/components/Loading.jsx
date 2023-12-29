fa2c54407a25110a90f3f31bed756aec89f83e33
Jerico <116884354+cout05@users.noreply.github.com>
2023-12-29 21:36:08 +0800

    change text color

--- frontend/src/components/Loading.jsx
+++ frontend/src/components/Loading.jsx
@@ -3,7 +3,7 @@ import React from "react";
 const Loading = () => {
   return (
     <div className=" flex items-center justify-center">
-      <div className=" h-32 w-32 text-[#f4f5f4]">
+      <div className=" h-32 w-32 text-[#235e86]">
         <h1 className="text-center text-2xl drop-shadow-md font-semibold">
           No Task...
         </h1>
