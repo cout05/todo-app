f7a0491c3ce960ffe1460322cae83be022ddeb26
Jerico <116884354+cout05@users.noreply.github.com>
2023-12-29 21:23:54 +0800

    updated

--- backend/index.js
+++ backend/index.js
@@ -2,6 +2,7 @@ import express from "express";
 import mongoose from "mongoose";
 import cors from "cors";
 import taskRoutes from "./routes/taskRoutes.js";
+import userRoutes from "./routes/userRoutes.js";
 import dotenv from "dotenv";
 
 dotenv.config();
@@ -9,12 +10,13 @@ dotenv.config();
 const app = express();
 app.use(express.json());
 
+app.use(cors());
+
 const MONGO_URI = process.env.MONGO_URI;
 const PORT = process.env.PORT;
 
-app.use(cors());
-
 app.use("/task", taskRoutes);
+app.use("/", userRoutes);
 
 mongoose
   .connect(MONGO_URI)
