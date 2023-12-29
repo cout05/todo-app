04daf843918dccd80124918fe76bc9f18afbfa1d
Jerico <116884354+cout05@users.noreply.github.com>
2023-12-29 21:35:57 +0800

    added routers

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,13 +1,22 @@
+import { BrowserRouter, Routes, Route, HashRouter } from "react-router-dom";
 import TodoAdd from "./components/TodoAdd";
 import TodoApp from "./components/TodoApp";
 import TodoUpdate from "./components/TodoUpdate";
 import TodoDelete from "./components/TodoDelete";
+import Login from "./components/Login";
+import Signup from "./components/Signup";
 import "./app.css";
 
 function App() {
   return (
     <div className="relative h-[100vh]">
-      <TodoApp />
+      <HashRouter basename="/">
+        <Routes>
+          <Route path="/" element={<Login />} />
+          <Route path="/signup" element={<Signup />} />
+          <Route path="/app" element={<TodoApp />} />
+        </Routes>
+      </HashRouter>
       <TodoAdd />
       <TodoDelete />
       <TodoUpdate />
