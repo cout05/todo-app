47e3eb9e4d5fd8565f1eeefdf7e671aaeaaa8b19
Jerico <116884354+cout05@users.noreply.github.com>
2023-12-30 12:26:36 +0800

    updated load state

--- frontend/src/components/Loading.jsx
+++ frontend/src/components/Loading.jsx
@@ -1,13 +1,13 @@
 import React from "react";
+import { VscFolderActive } from "react-icons/vsc";
 
 const Loading = () => {
   return (
-    <div className=" flex items-center justify-center">
-      <div className=" h-32 w-32 text-[#235e86]">
-        <h1 className="text-center text-2xl drop-shadow-md font-semibold">
-          No Task...
-        </h1>
-      </div>
+    <div className="flex flex-col items-center p-20 justify-center">
+      <VscFolderActive className="text-[#235e86] text-3xl drop-shadow-md" />
+      <h1 className="text-center text-[#235e86] text-2xl drop-shadow-md">
+        No Pending Task.
+      </h1>
     </div>
   );
 };
