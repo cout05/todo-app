e28521ea5f1f339c04fb04001ff5b86782cad557
Jerico <116884354+cout05@users.noreply.github.com>
2023-12-30 12:27:02 +0800

    added error handling

--- frontend/src/components/Login.jsx
+++ frontend/src/components/Login.jsx
@@ -3,6 +3,7 @@ import { Link } from "react-router-dom";
 import { FiEye } from "react-icons/fi";
 import { FiEyeOff } from "react-icons/fi";
 import { useNavigate } from "react-router-dom";
+import { IoPersonCircleOutline } from "react-icons/io5";
 import axios from "axios";
 
 const Login = () => {
@@ -12,21 +13,33 @@ const Login = () => {
   const navigate = useNavigate();
   const [eUname, setUname] = useState(false);
   const [epass, setEPass] = useState(false);
+  const [notFound, setNotFound] = useState(false);
+  const [iPass, setIPass] = useState(false);
 
   const apiUrl = import.meta.env.VITE_API_URL;
 
   const login = (event) => {
     event.preventDefault();
     if (username != "" && password != "") {
+      setUname(false);
+      setEPass(false);
       const data = {
         username: username,
         password: password,
       };
       axios
         .post(`${apiUrl}/login`, data)
-        .then(() => {
-          console.log("success");
-          navigate("/app");
+        .then((response) => {
+          if (response.data.message === "Login successful") {
+            navigate("/app");
+            console.log(response.data.message);
+          } else if (response.data.message === "User not found") {
+            setNotFound(true);
+            console.log(response.data.message);
+          } else {
+            setIPass(true);
+            console.log(response.data.message);
+          }
         })
         .catch((error) => {
           console.log(error);
@@ -47,8 +60,49 @@ const Login = () => {
 
   return (
     <div className="max-w-lg mx-auto p-4">
-      <div className="mt-24 bg-[#235e86] backdrop-blur-lg rounded-md px-10 py-5">
-        <h1 className="text-3xl drop-shadow-xl text-[#f4f5f4] font-bold text-center mb-4">
+      {!notFound ? null : (
+        <div
+          className="bg-red-100 border border-red-400 text-red-700 px-4 py-3 rounded relative"
+          role="alert">
+          <strong className="font-bold">Error! </strong>
+          <span className="block sm:inline">User not found.</span>
+          <span
+            onClick={() => setNotFound(false)}
+            className="absolute top-0 bottom-0 right-0 px-4 py-3">
+            <svg
+              className="fill-current h-6 w-6 text-red-500"
+              role="button"
+              xmlns="http://www.w3.org/2000/svg"
+              viewBox="0 0 20 20">
+              <title>Close</title>
+              <path d="M14.348 14.849a1.2 1.2 0 0 1-1.697 0L10 11.819l-2.651 3.029a1.2 1.2 0 1 1-1.697-1.697l2.758-3.15-2.759-3.152a1.2 1.2 0 1 1 1.697-1.697L10 8.183l2.651-3.031a1.2 1.2 0 1 1 1.697 1.697l-2.758 3.152 2.758 3.15a1.2 1.2 0 0 1 0 1.698z" />
+            </svg>
+          </span>
+        </div>
+      )}
+      {!iPass ? null : (
+        <div
+          className="bg-red-100 border border-red-400 text-red-700 px-4 py-3 rounded relative"
+          role="alert">
+          <strong className="font-bold">Error! </strong>
+          <span className="block sm:inline">Incorrect Password.</span>
+          <span
+            onClick={() => setIPass(false)}
+            className="absolute top-0 bottom-0 right-0 px-4 py-3">
+            <svg
+              className="fill-current h-6 w-6 text-red-500"
+              role="button"
+              xmlns="http://www.w3.org/2000/svg"
+              viewBox="0 0 20 20">
+              <title>Close</title>
+              <path d="M14.348 14.849a1.2 1.2 0 0 1-1.697 0L10 11.819l-2.651 3.029a1.2 1.2 0 1 1-1.697-1.697l2.758-3.15-2.759-3.152a1.2 1.2 0 1 1 1.697-1.697L10 8.183l2.651-3.031a1.2 1.2 0 1 1 1.697 1.697l-2.758 3.152 2.758 3.15a1.2 1.2 0 0 1 0 1.698z" />
+            </svg>
+          </span>
+        </div>
+      )}
+      <div className="mt-24 bg-gradient-to-b from-[#60b5d4] via-[#21597c] to-[#273d54] drop-shadow-xl rounded-md px-10 py-5">
+        <IoPersonCircleOutline className="text-5xl drop-shadow-xl text-[#f4f5f4] font-bold mx-auto" />
+        <h1 className="text-3xl drop-shadow-xl text-[#f4f5f4] font-semibold text-center mb-4">
           Login
         </h1>
         <form onSubmit={login} className="flex flex-col gap-4" method="post">
--- frontend/src/components/Signup.jsx
+++ frontend/src/components/Signup.jsx
@@ -4,6 +4,7 @@ import { FiEye } from "react-icons/fi";
 import { FiEyeOff } from "react-icons/fi";
 import axios from "axios";
 import { useNavigate } from "react-router-dom";
+import { IoPersonCircleOutline } from "react-icons/io5";
 
 const apiUrl = import.meta.env.VITE_API_URL;
 
@@ -16,11 +17,14 @@ const Signup = () => {
   const [eUname, setUname] = useState(false);
   const [epass, setEPass] = useState(false);
   const [ecpass, setECPass] = useState(false);
+  const [dupName, setDupName] = useState(false);
   const navigate = useNavigate();
 
   const signUp = (event) => {
     event.preventDefault();
     if (username !== "" && password !== "") {
+      setUname(false);
+      setEPass(false);
       if (password === cpass) {
         const data = {
           username: username,
@@ -29,11 +33,12 @@ const Signup = () => {
         axios
           .post(`${apiUrl}/signup`, data)
           .then(() => {
-            console.log("success");
+            setDupName(false);
             navigate("/");
           })
           .catch((error) => {
             console.log(error);
+            setDupName(true);
           });
       } else {
         setECPass(true);
@@ -54,7 +59,29 @@ const Signup = () => {
 
   return (
     <div className="max-w-lg mx-auto p-4">
-      <div className="mt-12 bg-[#235e86] backdrop-blur-lg rounded-md px-10 py-5">
+      {!dupName ? null : (
+        <div
+          className="bg-red-100 border border-red-400 text-red-700 px-4 py-3 rounded relative"
+          role="alert">
+          <strong className="font-bold">Error! </strong>
+          <span className="block sm:inline">Username already exist.</span>
+          <span
+            onClick={() => setDupName(false)}
+            className="absolute top-0 bottom-0 right-0 px-4 py-3">
+            <svg
+              className="fill-current h-6 w-6 text-red-500"
+              role="button"
+              xmlns="http://www.w3.org/2000/svg"
+              viewBox="0 0 20 20">
+              <title>Close</title>
+              <path d="M14.348 14.849a1.2 1.2 0 0 1-1.697 0L10 11.819l-2.651 3.029a1.2 1.2 0 1 1-1.697-1.697l2.758-3.15-2.759-3.152a1.2 1.2 0 1 1 1.697-1.697L10 8.183l2.651-3.031a1.2 1.2 0 1 1 1.697 1.697l-2.758 3.152 2.758 3.15a1.2 1.2 0 0 1 0 1.698z" />
+            </svg>
+          </span>
+        </div>
+      )}
+
+      <div className="mt-12 bg-gradient-to-b from-[#60b5d4] via-[#21597c] to-[#273d54] drop-shadow-xl rounded-md px-10 py-5">
+        <IoPersonCircleOutline className="text-5xl drop-shadow-xl text-[#f4f5f4] font-bold mx-auto" />
         <h1 className="text-3xl drop-shadow-xl text-[#f4f5f4] font-bold text-center mb-4">
           Signup
         </h1>
